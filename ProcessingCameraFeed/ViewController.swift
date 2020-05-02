524e868723521faf6f99977e3b5c3b1bea0469e6
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-05-02 22:38:37 +0100

    Initial commit: created new app project

---
+++ ProcessingCameraFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProcessingCameraFeed
+//
+//  Created by Anurag Ajwani on 02/05/2020.
+//  Copyright © 2020 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
