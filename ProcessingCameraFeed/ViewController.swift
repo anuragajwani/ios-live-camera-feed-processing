28918b3e48a29b1343237233078c4517acc645c5
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-05-02 22:42:25 +0100

    Now showing back camera feed to user

--- ProcessingCameraFeed/ViewController.swift
+++ ProcessingCameraFeed/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    private let captureSession = AVCaptureSession()
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        preview.videoGravity = .resizeAspect
+        return preview
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.addCameraInput()
+        self.addPreviewLayer()
+        self.captureSession.startRunning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.bounds
     }
 
-
+    private func addCameraInput() {
+        let device = AVCaptureDevice.default(for: .video)!
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private func addPreviewLayer() {
+        self.view.layer.addSublayer(self.previewLayer)
+    }
 }
 
